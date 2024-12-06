560bc2edf232e96b0ef37072cf1ee4290e276cef
Shift <shift@laravelshift.com>
2024-12-06 15:34:39 +0000

    Shift cleanup

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Auth;
 
 use App\Providers\AppServiceProvider;
 use App\Domain\Users\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
