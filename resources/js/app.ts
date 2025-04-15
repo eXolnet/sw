82720502b8c74c35c223bac4f91eb46063fa9691
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2025-04-15 16:47:10 -0400

    build(deps): update tightenco/ziggy to v2

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,7 @@ import '../scss/app.scss';
 import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
